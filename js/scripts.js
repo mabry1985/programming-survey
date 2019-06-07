a45e1cfc24682c7f6938dbf8c67b918edc87c06e
mabry1985 <mabry1985@gmail.com>
2019-06-07 11:03:26 -0700

    add more logic to scripts.js and some styling to styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,28 @@ $(document).ready(function() {
     var question4 = parseInt($("#question4").val());
 
     var match = question1 + question2 + question3 + question4;
+    console.log(match);
+    if (match < 6) {
+      $(".csharp").show();
+      $(".javascript, .swift").hide();
+    } else if (match < 10) {
+      $(".javascript").show();
+      $(".csharp, .swift").hide();
+    } else if (match < 14) {
+      $(".swift").show();
+      $(".javascript, .csharp,").hide();
+    } else if (match < 16) {
+      $(".swift, .javascript, .csharp,").hide();
+    } else {
+      alert("There was an error, please try again.");
+    }
+
+
+
+
+
+
+    $(".insert").text(name);
 
   });
 });
